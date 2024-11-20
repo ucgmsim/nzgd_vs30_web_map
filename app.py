f29c3fc1945c9f93f1e89e41f94943c5e49bf0d8
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-11-20 13:44:23 +1300

    added loading spinner

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
 from flask import Flask, render_template, request
+import geopandas as gpd
+from pathlib import Path
+import pandas as pd
+
+import folium
+from folium.plugins import MarkerCluster
+
 app = Flask(__name__)
 
 @app.route('/map', methods=['POST'])
 def map():
-    query = request.form['query']
-    # load the geopandas VS30 data frame and filter it based on the query
-    # make the folium map
-    return
+    query = request.form.get('query')
+
+    load_dir = Path("/home/arr65/data/nzgd/resources")
+    info_df = gpd.read_file(load_dir / "vs30_from_data.geojson")
+    if query:
+        info_df = info_df[info_df["record_name"] == query]
+
+    fmap = folium.Map(location=[-41.2728,173.2994], tiles="OpenStreetMap", zoom_start=6)
+
+    # Create a MarkerCluster object
+    marker_cluster = MarkerCluster().add_to(fmap)
+
+    for row_index, row in info_df.iterrows():
+        folium.Marker(
+            location=[row['latitude'], row['longitude']],
+            name=row["record_name"]
+        ).add_to(marker_cluster)
+
+    folium_map_render: str = fmap.get_root()._repr_html_()
+    # The following replace removes the erroneous request to trust the notebook.
+    return folium_map_render.replace(
+        '<span style="color:#565656">Make this Notebook Trusted to load map: File -> Trust Notebook</span>',
+        "",
+    )
+
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
     return render_template('index.html', title='VS30 Map')
 
 if __name__ == '__main__':
